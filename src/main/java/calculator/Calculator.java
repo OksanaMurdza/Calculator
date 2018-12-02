58aeb8428917ce2bb8b0b604ff0ac44574cc0fbd
Oksana Murdza <Oksanka.murdza@gfmail.com>
2018-12-02 14:23:33 +0200

    Calculator with change.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,80 +2,185 @@ package calculator;
 
 public class Calculator implements ExpressionCalculator {
 
-    private String str;
-
     public boolean isExpressionValid(String expression) {
         int num = 0;
-        str = expression;
-        int leng = str.length();
-        for (int i=0; i<leng; i++){
-            if (str.charAt(i) == '-' || str.charAt(i) == '+' || str.charAt(i) == '*' || str.charAt(i) == '/') {
-                if (str.charAt(i+1) == '-' || str.charAt(i+1) == '+' || str.charAt(i+1) == '*' || str.charAt(i+1) == '/') return false;
+        for (int i = 0; i < expression.length(); i++) {
+            if (!((expression.charAt(i) >= '(' && expression.charAt(i) <= '+') ||
+                    (expression.charAt(i) >= '-' && expression.charAt(i) <= '9'))) {
+                return false;
             }
-            if (str.charAt(i) == ')') num--;
-            if (str.charAt(i) == '(') num++;
+            if (expression.charAt(i) == '(') {
+                num++;
+            }
+
+            if (expression.charAt(i) == ')') {
+                if (num > 0) {
+                    num--;
+                } else {
+                    return false;
+                }
+            }
+
+            if (i == expression.length() - 1) {
+                if (num != 0) {
+                    return false;
+                }
+            }
+
+            if (Character.isDigit(expression.charAt(i))) {
+                int numPoint = 0;
+                while (true) {
+                    if (i == expression.length() - 1) {
+                        break;
+                    } else {
+                        i++;
+                    }
+                    if (Character.isDigit(expression.charAt(i)) || (expression.charAt(i) == '.' && numPoint == 0)) {
+                        if (expression.charAt(i) == '.') {
+                            numPoint++;
+                        }
+                        if (i == expression.length() - 1) {
+                            i--;
+                            break;
+                        }
+                    } else {
+                        if (numPoint > 1) {
+                            return false;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if (expression.charAt(i) == '-' || expression.charAt(i) == '+' ||
+                    expression.charAt(i) == '*' || expression.charAt(i) == '/') {
+                if (expression.charAt(i + 1) == '-' || expression.charAt(i + 1) == '+'
+                        || expression.charAt(i + 1) == '*' || expression.charAt(i + 1) == '/') {
+                    return false;
+                }
+            }
         }
-        return num == 0;
+        return true;
     }
 
-    public double calculateExpression(String expression) throws IllegalArgumentException{
-        if (!isExpressionValid(expression))
-            throw new IllegalArgumentException();
-        str = expression;
-        double result=0;
-        if (str.startsWith("(") && str.endsWith(")")) {
-            return calculateExpression(str.substring(1, str.length() - 1));
+    @Override
+    public double calculateExpression(String expression) throws IllegalArgumentException {
+        if (!isExpressionValid(expression)) {
+            throw new IllegalArgumentException(expression);
+        }
+        double leftSide;
+        double rightSide;
+        Double result = null;
+        int position = 0;
+
+        if (expression.charAt(position) == '-' && expression.charAt(position + 1) == '(') {
+            return -calculateExpression(expression.substring(1));
         } else {
-            int position = 0;
-            if (str.charAt(position) == '-' && str.charAt(position + 1) == '(') {
-                return calculateExpression(str.substring(1,str.length()-1));
-            } else {
-                if (str.charAt(position) == '-'){
-                    position++;
-                    while (position < str.length() - 1) {
-                        if (Character.isDigit(str.charAt(position)) || str.charAt(position) == '.'){
-                            position++;
-                        } else break;
+            if (expression.charAt(position) == '-') {
+                position++;
+                while (position < expression.length()) {
+                    if (Character.isDigit(expression.charAt(position)) || expression.charAt(position) == '.') {
+                        position++;
+                    } else {
+                        break;
                     }
-                    result = Double.parseDouble(str.substring(0, position));
+                }
+                result = Double.parseDouble(expression.substring(0, position));
+            }
+
+            if (expression.charAt(0) == '+') {
+                position++;
+            }
+
+
+            while (position < expression.length()) {
+
+                if (expression.charAt(position) == '(') {
+                    position++;
+                    int tmpPosition = position;
+                    int num = 1; //правильная ли скобка
+                    while (true) {
+                        if (expression.charAt(tmpPosition) == ')' && num == 1) {
+                            break;
+                        }
+                        if (expression.charAt(tmpPosition) == ')') {
+                            num--;
+                        }
+                        if (expression.charAt(tmpPosition) == '(') {
+                            num++;
+                        }
+                        tmpPosition++;
+                    }
+                    if (result != null) {
+                        result += calculateExpression(expression.substring(position, tmpPosition));
+                    } else {
+                        result = calculateExpression(expression.substring(position, tmpPosition));
+                    }
+                    position = tmpPosition;
+                    if (position == expression.length() - 1) {
+                        return result;
+                    }
+                    position++;
                 }
 
-                while (position < str.length()) {
-                    double leftSide;
-                    if (result != 0) {
-                        leftSide = result;
-                    } else {
-                        while (position < str.length()) {
-                            if (Character.isDigit(str.charAt(position)) || str.charAt(position) == '.') {
-                                position++;
-                            } else break;
+                if (result != null) {
+                    leftSide = result;
+                } else {
+                    while (position < expression.length()) {
+                        if (Character.isDigit(expression.charAt(position)) || expression.charAt(position) == '.') {
+                            position++;
+                        } else {
+                            break;
                         }
-                        leftSide = Double.parseDouble(str.substring(0, position));
                     }
-                    char operator = str.charAt(position);
-                    double rightSide;
+                    leftSide = Double.parseDouble(expression.substring(0, position));
+                    if (position == expression.length()) {
+                        return Double.parseDouble(expression);
+                    }
+                }
+                char operator = expression.charAt(position);
+
+                if (expression.charAt(position + 1) == '(') {
+                    position++;
+                    rightSide = calculateExpression(expression.substring(position));
+                    if (operator == '*') {
+                        return leftSide * rightSide;
+                    }
+                    if (operator == '/') {
+                        return leftSide / rightSide;
+                    }
+                    if (operator == '+') {
+                        return leftSide + rightSide;
+                    } else {
+                        return leftSide - rightSide;
+                    }
+                } else {
                     if (operator == '*' || operator == '/') {
                         position++;
                         int tmpPosition = position;
 
-                        while (position < str.length()) {
-                            if (Character.isDigit(str.charAt(position)) || str.charAt(position) == '.') {
+                        while (position < expression.length()) {
+                            if (Character.isDigit(expression.charAt(position)) || expression.charAt(position) == '.') {
                                 position++;
-                            } else break;
+                            } else {
+                                break;
+                            }
                         }
-                        rightSide = Double.parseDouble(str.substring(tmpPosition, position));
-                        if (operator == '*') result = leftSide * rightSide;
-                        if (operator == '/') result = leftSide / rightSide;
+                        rightSide = Double.parseDouble(expression.substring(tmpPosition, position));
+                        if (operator == '*') {
+                            result = leftSide * rightSide;
+                        }
+                        if (operator == '/') {
+                            result = leftSide / rightSide;
+                        }
 
 
                     } else {
-                        rightSide = calculateExpression(str.substring(position + 1));
+                        rightSide = calculateExpression(expression.substring(position + 1));
                         if (operator == '+') {
-                            result = leftSide + rightSide;
-                        } else  result = leftSide - rightSide;
-                        return result;
+                            return leftSide + rightSide;
+                        } else return leftSide - rightSide;
                     }
-
                 }
             }
         }
